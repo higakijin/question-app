4800d4c2417e650237182dbad530306a6c9a48da
higakijin <hm385.chejptks@gmail.com>
2021-10-01 11:03:46 +0900

    make controllers and models

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  namespace :admin do
+    get 'posts/index'
+    get 'posts/destroy'
+  end
+  namespace :admin do
+    get 'users/index'
+    get 'users/destroy'
+  end
+  # get 'answers/create'
+  # get 'posts/index'
+  # get 'posts/new'
+  # get 'posts/create'
+  # get 'posts/show'
+  # get 'posts/edit'
+  # get 'posts/destroy'
+  # get 'posts/index'
+  # get 'sessions/new'
+  # get 'sessions/create'
+  # get 'users/new'
+  # get 'users/create'
+  # get 'users/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
